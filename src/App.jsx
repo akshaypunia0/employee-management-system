6980a18dc1e9fdc80ac503a16400504a2119a9ac
akshaypunia0 <akshaypoonia00000@gmail.com>
2025-02-14 11:43:39 +0530

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,78 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './App.css'
 import Login from './Components/Auth/Login'
 import EmployeeDashboard from './Components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './Components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './Utils/LocalStorage'
+import { AuthContext } from './Context/AuthProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [user, setUser] = useState(null)
+
+  const authData = useContext(AuthContext)
+
+  console.log(authData);
+
+  // useEffect(() => {
+
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem('loggedInUser')
+
+  //     console.log(loggedInUser);
+
+  //     if(loggedInUser) {
+  //       console.log(loggedInUser.role);
+        
+  //       setUser(loggedInUser.role)
+  //     }
+  //   }
+
+  // }, [authData])
+
+  console.log(user);
+  
+
+
+
+  const handleLogin = (email, password) => {
+
+    if (authData && authData.admin.find((admin) => admin.email == email && admin.password == password)) {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+      setUser('admin')
+    }
+    else if (authData && authData.employees.find((employee) => employee.email == email && employee.password == password)) {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }))
+      setUser('employee')
+    }
+    else {
+      alert('Wrong crediantials');
+    }
+  }
+
+
+
+  // handleLogin(email, password)
+
+
+  // useEffect(() => {
+  //   setLocalStorage()
+  //   getLocalStorage()
+
+  // },)
+
 
   return (
     <>
       <div>
-        <Login />
-        {/* <EmployeeDashboard /> */}
-        {/* <AdminDashboard /> */}
+
+        {
+          !user ? <Login handleLogin={handleLogin} /> :
+            user == 'admin' ? <AdminDashboard /> :
+              user == 'employee' ? <EmployeeDashboard /> : null
+        }
+
+
       </div>
     </>
   )
